514f090b75bcc2a2dae34d46ad1b53263c7ddbb9
[Bruno] <[bruno.micalli@uol.com.br]>
2021-07-07 18:51:42 -0300

    Destaque feito e lista atualizada

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState} from 'react';
 import Tmdb from './Tmdb'
 import MovieRow from './Components/MovieRow';
+import FeatureMovie from './Components/FeatureMovie'
+
+
+import './App.css'; 
 
 
 export default () =>{
 
   const [movieList, SetMovieList]= useState([])
+
+  const [featuredData, setFeaturedData]= useState(null)
 
   useEffect(() =>{
     const loadAll = async ()=>{
@@ -13,12 +19,27 @@ export default () =>{
 
       let list = await Tmdb.getHomeList();
       SetMovieList(list)
+
+      //pegando o featured
+      let originals = list.filter(i=>i.slug === 'originals')
+      let randomChoosen = Math.floor(Math.random()  * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChoosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      setFeaturedData(chosenInfo)
+
+     
+      console.log(chosenInfo)
     }
 
     loadAll()
   }, []);
   return(
     <div className="page">
+
+      {featuredData &&
+      <FeatureMovie item={featuredData}/>
+      }
+
       <section className="lists">
       {movieList.map((item,key)=>(
       <MovieRow key={key} title={item.title} items={item.items} />
--- netflixclone/src/Components/MovieRow.js
+++ netflixclone/src/Components/MovieRow.js
@@ -4,12 +4,20 @@ import './MovieRow.css'
 
 export default({title, items}) =>{
     return(
-        <div>
+        <div className="movieRow">
             <h2>{title}</h2> 
             <div className="movie-row-area">
-                {items.results.length > 0 && items.results.map((item,key) =>(
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
-                ))}
+
+                <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item,key) =>(
+                        <div key={key} className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}  />
+                        </div>
+
+                    ))}
+                </div>
+
+                
             </div>
         </div>
     );
